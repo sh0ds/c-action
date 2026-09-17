2e3099c456efdd883109b9ce5cf4a8d49e5fceff
sh0ds <mail@sh0ds.dev>
2026-09-17 17:44:59 +0200

    smol readability upgrade

--- reverse_words/reverse_words.c
+++ reverse_words/reverse_words.c
@@ -11,21 +11,27 @@ void reverseWords(char* sentence, char* result) {
     while (sentence[i] != '\0') {
 
         while (sentence[i] == ' ') { i++; }
+
         if (sentence[i] == '\0') { break; }
+
         while (sentence[i] != ' ' && sentence[i] != '\0') {
             buffer[count][j] = sentence[i];
             i++;
             j++;
         }
+
         buffer[count][j] = '\0';
         count++;
         j = 0;
     }
+
     //PART 2: reverse words into result
-    if (count == 0) {strcpy(result, "\0"); return;}
+    if (count == 0) { strcpy(result, "\0"); return; }
+
     count--;
     strcpy(result, buffer[count]);
     count--;
+
     while (count >= 0) {
         strcat(result, " ");
         strcat(result, buffer[count]);
@@ -42,6 +48,7 @@ int main() {
     printf("Enter a sentence:\n");
     fgets(sentence, sizeof(sentence), stdin);
     sentence[strcspn(sentence, "\n")] = 0;
+
     printf("You entered: %s\n", sentence);
 
     reverseWords(sentence, result);
